c01118cb9401a0962eaff1e412837ffce2db933b
GGVishal <vishal.gadade14@gmail.com>
2023-01-28 20:19:23 +0530

    I have added Test_1....

---
+++ src/test/java/com/Ecommerece/TestClass/Test_1.java
@@ -0,0 +1,12 @@
+package com.Ecommerece.TestClass;
+
+import org.testng.annotations.Test;
+
+public class Test_1 {
+	
+	@Test
+	public void methodTest() {
+		System.out.println("Testing...........");
+	}
+	
+}
